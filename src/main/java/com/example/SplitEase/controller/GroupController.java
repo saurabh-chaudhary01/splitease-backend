9cc39b8de8c1709840cddeb757f0e525dcb0be5e
Saurabh Chaudhary <saurabh.ch169@gmail.com>
2025-07-09 18:39:50 +0530

    few api fixes

--- src/main/java/com/example/SplitEase/controller/GroupController.java
+++ src/main/java/com/example/SplitEase/controller/GroupController.java
@@ -3,9 +3,11 @@ package com.example.SplitEase.controller;
 import com.example.SplitEase.dto.request.ExpenseRequest;
 import com.example.SplitEase.dto.request.GroupRequest;
 import com.example.SplitEase.dto.response.GroupResponse;
+import com.example.SplitEase.entity.UserPrincipal;
 import com.example.SplitEase.service.GroupService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -16,24 +18,26 @@ import java.util.List;
 public class GroupController {
     private final GroupService groupService;
 
-    @PostMapping("/create-group")
+    @PostMapping("/new")
     @ResponseStatus(HttpStatus.CREATED)
     public GroupResponse createGroup(@RequestBody GroupRequest dto) {
         return groupService.createGroup(dto);
     }
 
-    @PostMapping("/add-expense")
+    @PostMapping("/expense")
     public void addExpense(@RequestBody ExpenseRequest dto) {
         groupService.addExpense(dto);
     }
 
-    @GetMapping("/user-groups/{userId}")
-    public List<GroupResponse> getUserGroups(@PathVariable Long userId) {
+    @GetMapping("/user-groups")
+    public List<GroupResponse> getUserGroups(@AuthenticationPrincipal UserPrincipal principal) {
+        Long userId = principal.getUserId();
         return groupService.getUserGroups(userId);
     }
 
-    @GetMapping("/member-groups/{userId}")
-    public List<GroupResponse> getUserMemberGroups(@PathVariable Long userId) {
+    @GetMapping("/member-groups")
+    public List<GroupResponse> getUserMemberGroups(@AuthenticationPrincipal UserPrincipal principal) {
+        Long userId = principal.getUserId();
         return groupService.getUserMemberGroups(userId);
     }
 }
