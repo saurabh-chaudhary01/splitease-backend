090f3c98422b2be1cef128f10f77114c95e5a2d2
Saurabh Chaudhary <saurabh.ch169@gmail.com>
2025-07-09 17:35:37 +0530

    first commit

---
+++ src/main/java/com/example/SplitEase/SplitEaseApplication.java
@@ -0,0 +1,13 @@
+package com.example.SplitEase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SplitEaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SplitEaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SplitEase/SplitEaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SplitEase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SplitEaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
