cddc3732a50aecc4515a184148b049afc382b7be
Saurabh Chaudhary <saurabh.ch169@gmail.com>
2025-07-14 15:01:02 +0530

    swagger and actuator added

---
+++ src/main/java/com/example/SplitEase/configuration/SwaggerConfig.java
@@ -0,0 +1,18 @@
+package com.example.SplitEase.configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI myCustomConfig() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("SplitEase App APIs")
+                        .description("By Saurabh Chaudhary")
+                );
+    }
+}
